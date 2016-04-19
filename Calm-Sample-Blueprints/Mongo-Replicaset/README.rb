60a78aad9f58f483e27c1ee8b70b878ea66e9fd6
Sarath Kumar <kumarsarath588@gmail.com>
2016-04-19 18:17:17 +0530

    Added mongo replicaset bluprint

---
+++ Calm-Sample-Blueprints/Mongo-Replicaset/README.rb
@@ -0,0 +1,11 @@
+# MONGO Replicaset Buleprint
+
+This blueprint created 3 node mongo replicaset.
+
+##OS
+	* Ubuntu
+
+## Quick Start
+	1. Upload the buleprint to calm.
+  	2. Change Aws provider in overview tab.
+	3. Change Credentials for DEMO under credentials tab.
