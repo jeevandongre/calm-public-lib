6505c37a65d42e87e26bd3b976ca2b2f4ac786f7
Jeevan Dongre <jeevandongre@Jeevans-MacBook-Pro.local>
2016-05-25 14:08:39 +0530

    Added Blueprints for Oracle Java8 and Java7. Tested on ubuntu 14.04

---
+++ Calm-Sample-Blueprints/Java-Oracle-7/JavaOracle7.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo add-apt-repository -yppa:webupd8team/java
+sudo apt-get -y update
+echo oracle-java7-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
+sudo apt-get install -y oracle-java7-installer
---
+++ Calm-Sample-Blueprints/Java-Oracle-8/README.md
@@ -0,0 +1,3 @@
+### Oracle Java 8 on Ubuntu 14.04
+
+#### Installs Oracle Java 8 on ubuntu 14.04 machine.
