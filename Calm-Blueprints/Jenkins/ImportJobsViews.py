6dc217c4c1b1f55c7c06c6b3165ec5415485eb2d
Sarath Kumar <kumarsarath588@gmail.com>
2016-06-10 12:22:45 +0530

    jenkins import jobs added support for 2.6

--- Calm-Blueprints/Jenkins/ImportJobsViews.py
+++ Calm-Blueprints/Jenkins/ImportJobsViews.py
@@ -2,17 +2,30 @@
 import jenkins,os
 
 jenkins_url='http://localhost:8080'
+def get_user_pass():
+  jenkins_user='admin'
+  with open('/var/lib/jenkins/secrets/initialAdminPassword') as password_file:
+    jenkins_pass=password_file.read().rstrip()
+  return jenkins_user,jenkins_pass
+
+def server_connect(jenkins_url):
+  if "@@{JENKINS_VERSION}@@" == "2.6":
+    jenkins_user,jenkins_pass=get_user_pass()
+    server = jenkins.Jenkins(jenkins_url,username=jenkins_user, password=jenkins_pass)
+  else:
+    server = jenkins.Jenkins(jenkins_url)
+  return server
 
 def create_job(jenkins_url,job_name,job_config):
-  server = jenkins.Jenkins(jenkins_url)
+  server=server_connect(jenkins_url)
   server.create_job(job_name, job_config)
 
 def create_view(jenkins_url,view_name,view_config):
-  server = jenkins.Jenkins(jenkins_url)
+  server=server_connect(jenkins_url)
   server.create_view(view_name,view_config)
 
 def check_job(job_name):
-  server = jenkins.Jenkins(jenkins_url)
+  server=server_connect(jenkins_url)
   jobs = server.get_jobs()
   for job in jobs:
     if job['fullname'] == job_name:
@@ -20,7 +33,7 @@ def check_job(job_name):
   return 0
 
 def check_view(view_name):
-  server = jenkins.Jenkins(jenkins_url)
+  server=server_connect(jenkins_url)
   views = server.get_views()
   for view in views:
     if view['name'] == view_name:
